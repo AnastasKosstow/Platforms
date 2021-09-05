0a772b9856469491c25ceda844e265497c9c53a6
Kosstow <anastas.kosstow@protonmail.com>
2021-09-05 15:00:29 +0300

    fix service, change ports

--- src/CommandsService.StartUp/Program.cs
+++ src/CommandsService.StartUp/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
-namespace Commands.StartUp
+namespace CommandsService.StartUp
 {
     public class Program
     {
