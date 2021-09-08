34982b828a3c07b30b714cf991d8365abbfe37a9
Kosstow <anastas.kosstow@protonmail.com>
2021-09-08 17:42:36 +0300

    CommandConfiguration

---
+++ src/CommandsService/CommandsService.Persistence/Configurations/CommandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CommandsService.Persistence.Models;
+
+namespace CommandsService.Persistence.Configurations
+{
+    public class CommandConfiguration : IEntityTypeConfiguration<Command>
+    {
+        public void Configure(EntityTypeBuilder<Command> builder)
+        {
+            builder
+                .HasOne(x => x.Platform)
+                .WithMany(x => x.Commands)
+                .HasForeignKey(x => x.PlatformId);
+        }
+    }
+}
