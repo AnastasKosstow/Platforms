d7cf42400d1e257c953814bd7a1f76dbaf15bdc1
Kosstow <anastas.kosstow@protonmail.com>
2021-09-09 10:21:01 +0300

    Guard

---
+++ src/CommandsService/CommandsService.Infrastructure/Guard/Guard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandsService.Infrastructure.Guard
+{
+    public static class Guard
+    {
+        public static void AgainstInvalidModel<TValue, TException>(TValue value)
+            where TException : BaseException, new()
+        {
+            if (value is not null)
+                return;
+
+            ThrowException<TException>($"Invalid model!");
+        }
+
+
+        private static void ThrowException<TException>(string message)
+            where TException : BaseException, new()
+        {
+            var exception = new TException
+            {
+                Error = message
+            };
+
+            throw exception;
+        }
+    }
+}
