77f723acd4a15f599f1723774b5c461fda809b69
Kosstow <anastas.kosstow@protonmail.com>
2021-09-10 05:20:37 +0300

    platform service controller refactoring

--- src/PlatformService/PlatformService.Web/Controllers/PlatformController.cs
+++ src/PlatformService/PlatformService.Web/Controllers/PlatformController.cs
@@ -20,26 +20,23 @@ namespace PlatformService.Web.Controllers
         [HttpGet]
         public async Task<GetPlatformsSuccessModel> GetPlatforms(
             CancellationToken cancellationToken)
-        {
-            return await _platformService.Get(cancellationToken);
-        }
+            =>
+            await _platformService.Get(cancellationToken);
 
 
         [HttpPost]
         public async Task<CreatePlatformSuccessModel> CreatePlatform(
             CreatePlatformRequestModel requestModel,
             CancellationToken cancellationToken)
-        {
-            return await _platformService.Create(requestModel, cancellationToken);
-        }
+            => 
+            await _platformService.Create(requestModel, cancellationToken);
 
 
         [HttpDelete]
         public async Task<DeletePlatformSuccessModel> DeletePlatform(
             DeletePlatformRequestModel requestModel,
             CancellationToken cancellationToken)
-        {
-            return await _platformService.Delete(requestModel, cancellationToken);
-        }
+            => 
+            await _platformService.Delete(requestModel, cancellationToken);
     }
 }
