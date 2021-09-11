257dcded3c5c518e374a4e829e83862aae9a78af
Kosstow <anastas.kosstow@protonmail.com>
2021-09-11 20:27:46 +0300

    refactoring

--- src/PlatformService/PlatformService.Messaging/MessageBusClient.cs
+++ src/PlatformService/PlatformService.Messaging/MessageBusClient.cs
@@ -2,6 +2,7 @@
 using PlatformService.Messaging.Configuration;
 using PlatformService.Messaging.Models;
 using RabbitMQ.Client;
+using System.Text.Json;
 
 namespace PlatformService.Messaging
 {
@@ -10,31 +11,45 @@ namespace PlatformService.Messaging
         private IConnection _connection;
         private IModel _channel;
 
+        private readonly RabbitMqConfiguration _rabbitMq;
+
         public MessageBusClient(IOptions<RabbitMqConfiguration> rabbitMq)
         {
-            var rabbitMqConfiguration = rabbitMq.Value;
+            _rabbitMq = rabbitMq.Value;
+        }
+            
+
+        public void Publish(PlatformPublishModel platformPublishModel)
+        {
+            var body = JsonSerializer.SerializeToUtf8Bytes(platformPublishModel);
 
             var factory = new ConnectionFactory
             {
-                HostName = rabbitMqConfiguration.RabbitMqHost,
-                Port = int.Parse(rabbitMqConfiguration.RabbitMqPort)
+                HostName = _rabbitMq.RabbitMqHost,
+                Port = int.Parse(_rabbitMq.RabbitMqPort)
             };
 
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
 
             _channel.ExchangeDeclare(
                 exchange: "trigger",
                 type: ExchangeType.Fanout);
 
             _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
-        }
-            
 
-        public void Publish(PlatformPublishModel platformPublishModel)
-        {
-            
+            channel.QueueDeclare
+                (queue: "", 
+                exclusive: false, 
+                autoDelete: false);
+
+            channel.BasicPublish(
+                exchange: "trigger", 
+                routingKey: "", 
+                basicProperties: null, 
+                body: body);
         }
+
 
         private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
         {
