8541278bc99055e4d5c08a8ff742d05c9d19fd0c
Kosstow <anastas.kosstow@protonmail.com>
2021-09-12 07:54:19 +0300

    EventType

---
+++ src/CommandsService/CommandsService.Messaging/EventProcessing/EventProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace CommandsService.Messaging.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        public Task ProcessEvent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
